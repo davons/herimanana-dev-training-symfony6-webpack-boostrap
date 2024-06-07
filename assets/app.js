b9ad4cbccee9754fee4eea3633f7a4ed4e10d21a
Yvon Rakotonanahary <yvon.rakotonanahary@gmail.com>
2024-06-07 07:47:05 +0200

    Login form

--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,5 @@
-import './app.scss'
+import './styles/scss/app.scss'
+import * as bootstrap from 'bootstrap'
+import "@fortawesome/fontawesome-free/js/all.js";
+import "@fortawesome/fontawesome-free/css/all.css";
+//import '@fortawesome/fontawesome-free/js/all'
